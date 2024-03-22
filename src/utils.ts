aa445fd2194a6b821faad7d4ad2ce4e9c0badcb4
Justin Bird <justin@thewordisbird.dev>
2024-03-22 13:52:53 -0700

    Remove unused utils.ts file

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function writeToLocalStorage(key: string, value: string): void {
-	localStorage.setItem(key, value);
-}
-
-export function readFromLocalStorage(key: string): string | null {
-	return localStorage.getItem(key);
-}
-
-export function removeFromLocalStorage(key: string): void {}
